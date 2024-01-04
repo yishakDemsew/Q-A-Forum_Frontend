59bb12fe50574e28937569484b001cd8a080f4ae
Yishak <yishakdemsew@gmail.com>
2024-01-04 20:56:05 +0300

    added an api prefix in the baseURL

--- src/AxiosConfig/AxiosConfig.js
+++ src/AxiosConfig/AxiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const axiosBase = axios.create({
     // baseURL: "http://localhost:5500/api",
-    baseURL: "https://q-a-forum-backend.onrender.com/",
+    baseURL: "https://q-a-forum-backend.onrender.com/api",
 });
 
 export default axiosBase;
