8153559ead753cb6ee26e2408daa29e70ea6c8fa
Yishak <yishakdemsew@gmail.com>
2024-01-04 19:12:36 +0300

    updated the baseURL in the axiosConfing file

--- src/AxiosConfig/AxiosConfig.js
+++ src/AxiosConfig/AxiosConfig.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const axiosBase = axios.create({
-    baseURL: "http://localhost:5500/api",
+    // baseURL: "http://localhost:5500/api",
+    baseURL: "https://q-a-forum-backend.onrender.com/",
 });
 
 export default axiosBase;
